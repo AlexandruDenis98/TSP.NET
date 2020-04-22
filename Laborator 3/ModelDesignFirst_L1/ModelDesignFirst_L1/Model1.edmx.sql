deaef0af7bc142f63dd8fc4ff1f90464daceab4e
Alexandru <alexandru98d@gmail.com>
2020-04-22 20:19:55 +0300

    Adding laborator 3

---
+++ Laborator 3/ModelDesignFirst_L1/ModelDesignFirst_L1/Model1.edmx.sql
@@ -0,0 +1,176 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 04/22/2020 19:48:28
+-- Generated from EDMX file: C:\Users\alexa\OneDrive\Desktop\ModelDesignFirst_L1 backup\ModelDesignFirst_L1\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [TestPerson];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_CustomerOrder]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_CustomerOrder];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[People]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[People];
+GO
+IF OBJECT_ID(N'[dbo].[Customers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customers];
+GO
+IF OBJECT_ID(N'[dbo].[Orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Orders];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'People'
+CREATE TABLE [dbo].[People] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(10)  NOT NULL,
+    [MiddleName] nvarchar(10)  NOT NULL,
+    [LastName] nvarchar(10)  NOT NULL,
+    [TelephonNumber] nvarchar(10)  NOT NULL
+);
+GO
+
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [CustomerId] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(50)  NOT NULL,
+    [City] nvarchar(50)  NOT NULL
+);
+GO
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [OrderId] int IDENTITY(1,1) NOT NULL,
+    [TotalValue] decimal(18,0)  NOT NULL,
+    [Date] datetime  NOT NULL,
+    [CustomerCustomerId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Albums'
+CREATE TABLE [dbo].[Albums] (
+    [AlbumId] int IDENTITY(1,1) NOT NULL,
+    [AlbumName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Artists'
+CREATE TABLE [dbo].[Artists] (
+    [ArtistId] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'AlbumArtist'
+CREATE TABLE [dbo].[AlbumArtist] (
+    [Albums_AlbumId] int  NOT NULL,
+    [Artists_ArtistId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'People'
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [PK_People]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [CustomerId] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC);
+GO
+
+-- Creating primary key on [OrderId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([OrderId] ASC);
+GO
+
+-- Creating primary key on [AlbumId] in table 'Albums'
+ALTER TABLE [dbo].[Albums]
+ADD CONSTRAINT [PK_Albums]
+    PRIMARY KEY CLUSTERED ([AlbumId] ASC);
+GO
+
+-- Creating primary key on [ArtistId] in table 'Artists'
+ALTER TABLE [dbo].[Artists]
+ADD CONSTRAINT [PK_Artists]
+    PRIMARY KEY CLUSTERED ([ArtistId] ASC);
+GO
+
+-- Creating primary key on [Albums_AlbumId], [Artists_ArtistId] in table 'AlbumArtist'
+ALTER TABLE [dbo].[AlbumArtist]
+ADD CONSTRAINT [PK_AlbumArtist]
+    PRIMARY KEY CLUSTERED ([Albums_AlbumId], [Artists_ArtistId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [CustomerCustomerId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [FK_CustomerOrder]
+    FOREIGN KEY ([CustomerCustomerId])
+    REFERENCES [dbo].[Customers]
+        ([CustomerId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CustomerOrder'
+CREATE INDEX [IX_FK_CustomerOrder]
+ON [dbo].[Orders]
+    ([CustomerCustomerId]);
+GO
+
+-- Creating foreign key on [Albums_AlbumId] in table 'AlbumArtist'
+ALTER TABLE [dbo].[AlbumArtist]
+ADD CONSTRAINT [FK_AlbumArtist_Album]
+    FOREIGN KEY ([Albums_AlbumId])
+    REFERENCES [dbo].[Albums]
+        ([AlbumId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Artists_ArtistId] in table 'AlbumArtist'
+ALTER TABLE [dbo].[AlbumArtist]
+ADD CONSTRAINT [FK_AlbumArtist_Artist]
+    FOREIGN KEY ([Artists_ArtistId])
+    REFERENCES [dbo].[Artists]
+        ([ArtistId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_AlbumArtist_Artist'
+CREATE INDEX [IX_FK_AlbumArtist_Artist]
+ON [dbo].[AlbumArtist]
+    ([Artists_ArtistId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ Laborator 3/ModelDesignFirst_L1/ModelDesignFirst_L1/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDesignFirst_L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Model Designer First");
+            TestPerson();
+            TesTOneToMany();
+            TestManyToMany();
+            Console.ReadKey();
+        }
+        static void TestPerson()
+        {
+            using (Model1Container context = new Model1Container())
+            {
+                string firstName = Console.ReadLine();
+                string lastName = Console.ReadLine();
+                string middleName = Console.ReadLine();
+                string telephonNumber = Console.ReadLine();
+
+                Person p = new Person()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    MiddleName = middleName,
+                    TelephonNumber = telephonNumber
+                };
+                context.People.Add(p);
+                context.SaveChanges();
+                var items = context.People;
+                foreach (var x in items)
+                    Console.WriteLine("{0} {1}", x.Id, x.FirstName);
+            }
+        }
+        static void TesTOneToMany()
+        {
+            Console.WriteLine("One to many association");
+            string name, city;
+            decimal totalValue;
+            DateTime date;
+            using (Model1Container context =
+           new Model1Container())
+            {
+                Console.WriteLine("Insert the name of the customer: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Insert the city of the customer: ");
+                city = Console.ReadLine();
+                Customer c = new Customer()
+                {
+                    Name = name,
+                    City = city
+                };
+                Console.WriteLine("Insert the totalValue of the order: ");
+                totalValue = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Insert the date of the order: ");
+                date = DateTime.Parse(Console.ReadLine());
+                Order o1 = new Order()
+                {
+                    TotalValue = totalValue,
+                    Date = date,
+                    Customer = c
+                };
+                Console.WriteLine("Insert the totalValue of the order: ");
+                totalValue = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Insert the date of the order: ");
+                date = Convert.ToDateTime(Console.ReadLine());
+                Order o2 = new Order()
+                {
+                    TotalValue = totalValue,
+                    Date = date,
+                    Customer = c
+                };
+                context.Customers.Add(c);
+                context.Orders.Add(o1);
+                context.Orders.Add(o2);
+                context.SaveChanges();
+                var items = context.Customers;
+                foreach (var x in items)
+                {
+                    Console.WriteLine("Customer : {0}, {1}, {2}",
+                   x.CustomerId, x.Name, x.City);
+                    foreach (var ox in x.Orders)
+                        Console.WriteLine("\tOrders: {0}, {1}, {2}",
+                       ox.OrderId, ox.Date, ox.TotalValue);
+                }
+            }
+        }
+        static void TestManyToMany()
+        {
+            Console.WriteLine("Many to many association");
+            //string albumName, firstName, lastName;
+
+            using (Model1Container context =
+           new Model1Container())
+            {
+                //Console.WriteLine("Insert the name of the album: ");
+                //albumName = Console.ReadLine();
+                Album album1 = new Album()
+                {
+                    AlbumName = "album1"
+                };
+                Album album2 = new Album()
+                {
+                    AlbumName = "album2"
+                };
+
+                //Console.WriteLine("Insert the first name of the artist: ");
+                //firstName= Console.ReadLine();
+                //Console.WriteLine("Insert the last name of the artist: ");
+                //lastName = Console.ReadLine();
+
+                Artist artist1 = new Artist()
+                {
+                     FirstName = "firstName1",
+                     LastName = "lastName1"
+                };
+                Artist artist2 = new Artist()
+                {
+                    FirstName = "firstName2",
+                    LastName = "lastName2"
+                };
+
+                context.Artists.Add(artist1);
+                context.Artists.Add(artist2);
+                context.Albums.Add(album1);
+                context.Albums.Add(album2);
+                context.SaveChanges();
+
+                //context.Albums.Add(album);
+                var artists = context.Artists;
+                foreach (var artist in artists)
+                {
+                    Console.WriteLine("Artist : {0}, {1}, {2}",
+                   artist.ArtistId, artist.FirstName, artist.LastName);
+                }
+                var albums = context.Albums;
+                foreach (var album in albums)
+                    Console.WriteLine("\tOrders: {0}, {1}",
+                   album.AlbumId, album.AlbumName);
+            }
+        }
+
+    }
+
+}
